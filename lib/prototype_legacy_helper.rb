e9f2ef627c009833f8044c51319d78ca188c00b8
Joe Vennix <joevennix@gmail.com>
2011-12-05 16:00:49 -0600

    Added missing helper methods from rails 3.0.9 to bring up to date with rails 3.1

--- lib/prototype_legacy_helper.rb
+++ lib/prototype_legacy_helper.rb
@@ -1,4 +1,86 @@
 module PrototypeHelper
+################### BEGIN COPYPASTA OF RAILS 3.0.9 CODE ###################
+  CALLBACKS    = Set.new([ :uninitialized, :loading, :loaded,
+                   :interactive, :complete, :failure, :success ] +
+                   (100..599).to_a)
+  AJAX_OPTIONS = Set.new([ :before, :after, :condition, :url,
+                   :asynchronous, :method, :insertion, :position,
+                   :form, :with, :update, :script ]).merge(CALLBACKS)
+
+  def build_callbacks(options)
+    callbacks = {}
+    options.each do |callback, code|
+      if CALLBACKS.include?(callback)
+        name = 'on' + callback.to_s.capitalize
+        callbacks[name] = "function(request){#{code}}"
+      end
+    end
+    callbacks
+  end
+
+  def method_option_to_s(method)
+    (method.is_a?(String) and !method.index("'").nil?) ? method : "'#{method}'"
+  end
+
+  def options_for_ajax(options)
+    js_options = build_callbacks(options)
+
+    js_options['asynchronous'] = options[:type] != :synchronous
+    js_options['method']       = method_option_to_s(options[:method]) if options[:method]
+    js_options['insertion']    = "'#{options[:position].to_s.downcase}'" if options[:position]
+    js_options['evalScripts']  = options[:script].nil? || options[:script]
+
+    if options[:form]
+      js_options['parameters'] = 'Form.serialize(this)'
+    elsif options[:submit]
+      js_options['parameters'] = "Form.serialize('#{options[:submit]}')"
+    elsif options[:with]
+      js_options['parameters'] = options[:with]
+    end
+
+    if protect_against_forgery? && !options[:form]
+      if js_options['parameters']
+        js_options['parameters'] << " + '&"
+      else
+        js_options['parameters'] = "'"
+      end
+      js_options['parameters'] << "#{request_forgery_protection_token}=' + encodeURIComponent('#{escape_javascript form_authenticity_token}')"
+    end
+
+    options_for_javascript(js_options)
+  end
+
+  def remote_function(options)
+    javascript_options = options_for_ajax(options)
+
+    update = ''
+    if options[:update] && options[:update].is_a?(Hash)
+      update  = []
+      update << "success:'#{options[:update][:success]}'" if options[:update][:success]
+      update << "failure:'#{options[:update][:failure]}'" if options[:update][:failure]
+      update  = '{' + update.join(',') + '}'
+    elsif options[:update]
+      update << "'#{options[:update]}'"
+    end
+
+    function = update.empty? ?
+      "new Ajax.Request(" :
+      "new Ajax.Updater(#{update}, "
+
+    url_options = options[:url]
+    function << "'#{html_escape(escape_javascript(url_for(url_options)))}'"
+    function << ", #{javascript_options})"
+
+    function = "#{options[:before]}; #{function}" if options[:before]
+    function = "#{function}; #{options[:after]}"  if options[:after]
+    function = "if (#{options[:condition]}) { #{function}; }" if options[:condition]
+    function = "if (confirm('#{escape_javascript(options[:confirm])}')) { #{function}; }" if options[:confirm]
+
+    return function.html_safe
+  end
+
+  ###################  END COPYPASTA OF RAILS 3.0.9 CODE  ###################
+
   # Creates a button with an onclick event which calls a remote action
   # via XMLHttpRequest
   # The options for specifying the target with :url
